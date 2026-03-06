982feea6cd04642995115cb110c63465950353b6
Caleb <caleb.hunt789@gmail.com>
2026-03-06 17:35:01 +0000

    config is setup

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+$host = "localhost"; 
+$db = "teamtracker"; 
+$user = "root"; 
+$password = ""; 
+
+//points to the database
+$dsn = "mysql:host=$host;dbname=$db";
+
+
+try {
+   $pdo = new PDO ($dsn, $user, $password); 
+   $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+}
+//checking if error in the connection
+catch(PDOException $e) {
+    die("Database connection failed: " . $e->getMessage()); 
+}
+?>
